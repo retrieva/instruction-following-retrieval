de8efd4656c60cbfe4352f8488d356751a00956b
sugiyamaseiji <seiji.sugiyama@retrieva.jp>
2025-09-19 11:52:10 +0900

    upload for multi gpu training

--- script/05-multi-gpu-con-basic.sh
+++ script/05-multi-gpu-con-basic.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #PJM -L rscgrp=b-batch
-#PJM -L gpu=1
+#PJM -L gpu=2
 #PJM -L elapse=05:00:00
 #PJM -j
 #PJM -o ./logs/05-3B-con.txt
@@ -15,8 +15,8 @@ deepspeed ./src/multi-gpu-train.py \
  model.base_model_name="meta-llama/Llama-3.2-3B-Instruct" \
  wandb.name="05-3B-con" \
  wandb.use_wandb=true \
- loss.contrastive_loss=false \
+ loss.contrastive_loss=true \
  loss.weighted_contrastive_loss_v1=false \
  loss.weighted_contrastive_loss_v2=false \
- loss.margin_loss=true \
+ loss.margin_loss=false \
  training.output_dir=${SAVE_MODEL} 
