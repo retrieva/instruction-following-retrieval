b151286e12c17023e7f49a57621b998a19ef1f29
sugiyamaseiji <seiji.sugiyama@retrieva.jp>
2025-09-19 12:06:00 +0900

    upload for multi gpu training

--- script/05-multi-gpu-weight-v2.sh
+++ script/05-multi-gpu-weight-v2.sh
@@ -3,21 +3,21 @@
 #PJM -L gpu=2
 #PJM -L elapse=03:00:00
 #PJM -j
-#PJM -o ./logs/05-3B-con.txt
+#PJM -o ./logs/05-3B-weight-v2.txt
 
 module load cuda cudnn nccl gcc
 . .venv/bin/activate
 
-SAVE_MODEL="./save_model/05-3B-con"
-RESULT_PATH="./results/05-3B-con"
+SAVE_MODEL="./save_model/05-3B-weight-v2"
+RESULT_PATH="./results/05-3B-weight-v2"
 
 deepspeed ./src/multi-gpu-train.py \
  model.base_model_name="meta-llama/Llama-3.2-3B-Instruct" \
- wandb.name="05-3B-con" \
+ wandb.name="05-3B-weight-v2" \
  wandb.use_wandb=true \
- loss.contrastive_loss=true \
+ loss.contrastive_loss=false \
  loss.weighted_contrastive_loss_v1=false \
- loss.weighted_contrastive_loss_v2=false \
+ loss.weighted_contrastive_loss_v2=true \
  loss.margin_loss=false \
  training.output_dir=${SAVE_MODEL} 
 
